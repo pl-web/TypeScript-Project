ca73b3dcd48fe38e51d87adc4451e246db6cdcfc
it-reader <hnature@126.com>
2020-10-18 10:51:48 +0800

    数组

--- test/04.ts
+++ test/04.ts
@@ -2,3 +2,11 @@
 const new_arr: Array<number> = [1, 2, 3];
 const str_arr: Array<string> = ['1', '2', '3'];
 const arr: (number | string)[] = [1, '1'];
+const obj_arr: {name: string, age: number}[] = [
+  {name: 'Jack', age: 1}
+]
+// 类型别名
+type Person = {name: string, age: number};
+const person_arr: Person[] = [
+  { name: 'Jane', age: 1 }
+]
