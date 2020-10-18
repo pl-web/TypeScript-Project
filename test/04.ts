248b568e7cdac339f911f9b4f88144c5ba7259bc
it-reader <hnature@126.com>
2020-10-18 10:57:44 +0800

    元组

--- test/04.ts
+++ test/04.ts
@@ -1,12 +1,18 @@
-// 数组和元组
+// 数组n Array
 const new_arr: Array<number> = [1, 2, 3];
 const str_arr: Array<string> = ['1', '2', '3'];
 const arr: (number | string)[] = [1, '1'];
-const obj_arr: {name: string, age: number}[] = [
-  {name: 'Jack', age: 1}
+const obj_arr: { name: string, age: number }[] = [
+  { name: 'Jack', age: 1 }
 ]
 // 类型别名
-type Person = {name: string, age: number};
+type Person = { name: string, age: number };
 const person_arr: Person[] = [
   { name: 'Jane', age: 1 }
 ]
+
+// 元组 Tuple
+const userInfo = [
+  { name: 'Jenny', age: 1, id: '00001' }
+];
+const user: [string, string, number] = ['jenny', '00001', 1];
